a08dc042fc18f7c31443eda0c75be0eb866db80f
Untitled <john.d.reddock@gmail.com>
2018-01-28 15:47:57 -0500

    assignment 1 - added 3 routers

---
+++ routes/promoRouter.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })  
+  .get((req,res,next)=> {
+      res.end('Will send all the promotions to you!');
+  })  
+  .post((req,res,next)=> {
+      res.end('Will add the promotion: ' + req.body.name + ' with details: ' + 
+    req.body.description);
+  })  
+  .put((req,res,next)=> {
+      res.statusCode = 403;
+      res.end('PUT operation not supported on /promotions');
+  })  
+  .delete((req,res,next)=> {    
+    res.end('Deleting all promotions');
+  });
+  
+module.exports = promoRouter;
+
+//dish ID section
+// app.get('/dishes/:dishId', (req,res,next)=> {
+//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+// });
+
+// app.post('/dishes/:dishId', (req,res,next)=> {
+//     res.statusCode = 403;
+//     res.end('POST operation not supported on /dishes/' + req.params.dishId);
+// });
+
+// app.put('/dishes/:dishId', (req,res,next)=> {
+//     res.write('Updating the dish' + req.params.dishId + '\n');
+//     res.end('Will update the dish: ' + req.body.name + 
+//           'with details' + req.body.description);
+// });
+
+// app.delete('/dishes/:dishId', (req,res,next)=> {    
+//   res.end('Deleting dish: ' + req.params.dishId);
+// });
+// app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res, next) => {    
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+// });
+
+// const server = http.createServer(app);
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+
+// });
